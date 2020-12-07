c0734e15f73bd14ca68703ee918307c24cb932be
Sitt Aung <sittaung07@gmail.com>
2020-12-07 22:36:34 +0630

    Fetching Products from React(useEffect)

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -70,7 +70,7 @@ Rating.defaultProps = {
 Rating.propTypes = {
   value: PropTypes.number.isRequired,
   text: PropTypes.string.isRequired,
-  color: PropTypes.string
+  color: PropTypes.string,
 }
 
 export default Rating
