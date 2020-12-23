28189dcf29c820c2633eb21f41edba450a233cfd
Sitt Aung <sittaung07@gmail.com>
2020-12-23 23:33:49 +0630

    Add To Cart Functionality

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,5 +1,3 @@
-import PropTypes from 'prop-types';
-
 const Rating = ({ value, text, color }) => {
   return (
     <div className='rating'>
@@ -65,12 +63,6 @@ const Rating = ({ value, text, color }) => {
 
 Rating.defaultProps = {
   color: '#f8e825'
-}
-
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string,
 }
 
 export default Rating
